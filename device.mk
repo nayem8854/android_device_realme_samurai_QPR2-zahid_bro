d7c8b60f55ede286bb5fc90b077b6459febcbcc3
Kelvin Zhang <zhangkelvin@google.com>
2024-10-27 16:17:56 +0100

    samurai: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -482,6 +482,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samurai
 
+# Update
+AB_OTA_UPDATER := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
