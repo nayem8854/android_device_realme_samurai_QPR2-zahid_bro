772ffde879b248c9950461026f6c058644196fa4
Kelvin Zhang <zhangkelvin@google.com>
2024-11-18 20:36:24 +0530

    samurai: Allow to access bootable/deprecated-ota targets
    sm8150-common is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to sm8150-common namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816

--- device.mk
+++ device.mk
@@ -383,6 +383,7 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_SOONG_NAMESPACES += \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
