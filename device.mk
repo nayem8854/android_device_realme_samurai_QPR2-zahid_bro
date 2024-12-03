ba40ca3fbb24cd986252ea1ba65dee2e32418cc0
Cyborg <cyborgdc2000@gmail.com>
2024-12-03 19:09:03 +0530

    samurai: overlay: Move dialer call record to rro

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigRes \
+    DialerRes \
     FrameworksRes \
     LineageSDKOverlay \
     SettingsProviderRes \
