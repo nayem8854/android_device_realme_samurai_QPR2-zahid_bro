99b4648debb5634376b96c6427eb70ce419982ef
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-12 22:52:31 +0530

    samurai: Stop building android.hardware.bluetooth.a2dp@1.0.vendor

--- device.mk
+++ device.mk
@@ -102,7 +102,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     audio.primary.msmnile \
     audio.bluetooth.default \
