ea1dc9165808ab1a4b9959a09c8f091fd1c92120
basamaryan <basam.aryan@gmail.com>
2025-07-26 22:54:19 +0530

    samurai: Drop legacy ANT remnants QSSI only has platform/external/ant-wireless/hidl since a while

--- device.mk
+++ device.mk
@@ -93,11 +93,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant@1.0.vendor
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
