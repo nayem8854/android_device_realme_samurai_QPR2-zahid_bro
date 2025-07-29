f63aed13bdd0df0ad3741145b8d3ed41142011bd
Cyborg <cyborgdc2000@gmail.com>
2025-07-29 19:45:50 +0530

    samurai: Drop TouchscreenGestures

--- device.mk
+++ device.mk
@@ -496,10 +496,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samurai
-
 # Update
 AB_OTA_UPDATER := false
 
