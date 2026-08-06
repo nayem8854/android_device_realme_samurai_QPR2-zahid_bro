ae3d1e5c4a8d63942eadddc9300d6452704b29fe
zahid5656 <zahid5656@gmail.com>
2026-08-06 10:22:11 +0600

    samurai: update ALS capture for Android 16 graphics API

--- als/default/AreaCapture.cpp
+++ als/default/AreaCapture.cpp
@@ -6,7 +6,6 @@
 
 #include "AreaCapture.h"
 
-#include <gui/AidlUtil.h>
 #include <gui/SurfaceComposerClient.h>
 #include <gui/SyncScreenCaptureListener.h>
 #include <ui/DisplayState.h>
@@ -21,7 +20,6 @@ using android::sp;
 using android::SurfaceComposerClient;
 using android::SyncScreenCaptureListener;
 using android::gui::ScreenCaptureResults;
-using android::gui::aidl_utils::toARect;
 using android::ui::PixelFormat;
 
 namespace aidl {
@@ -56,10 +54,13 @@ ndk::ScopedAStatus AreaCapture::getAreaBrightness(AreaRgbCaptureResult* _aidl_re
     DisplayCaptureArgs displayCaptureArgs;
     displayCaptureArgs.displayToken = getInternalDisplayToken();
    displayCaptureArgs.captureArgs.pixelFormat = ::android::PIXEL_FORMAT_RGBA_8888;
-    displayCaptureArgs.captureArgs.sourceCrop = toARect(m_screenshot_rect);
+    displayCaptureArgs.captureArgs.sourceCrop.left = m_screenshot_rect.left;
+    displayCaptureArgs.captureArgs.sourceCrop.top = m_screenshot_rect.top;
+    displayCaptureArgs.captureArgs.sourceCrop.right = m_screenshot_rect.right;
+    displayCaptureArgs.captureArgs.sourceCrop.bottom = m_screenshot_rect.bottom;
     displayCaptureArgs.width = m_screenshot_rect.getWidth();
     displayCaptureArgs.height = m_screenshot_rect.getHeight();
-    displayCaptureArgs.captureArgs.captureSecureLayers = true;
+    displayCaptureArgs.captureArgs.secureLayerMode = ::android::gui::SecureLayerMode::Capture;
 
     sp<SyncScreenCaptureListener> captureListener = new SyncScreenCaptureListener();
     if (ScreenshotClient::captureDisplay(displayCaptureArgs, captureListener) !=
