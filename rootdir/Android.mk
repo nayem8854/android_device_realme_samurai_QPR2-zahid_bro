9c73f87833a719251cc92ae6c703f8edb1fd9351
MD. ZAHIDUL ISLAM <74774199+zahid5656@users.noreply.github.com>
2026-08-14 17:04:31 +0600

    samurai: package zram init policy

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE        := fstab.qcom
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/fstab.qcom
-LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk init.zram.rc
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
@@ -16,6 +16,14 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := init.zram.rc
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := etc/init.zram.rc
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
