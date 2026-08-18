3042096e920590faadd7714f17d9baebb9ef708a
Zannatun Nayem <znayem830@gmail.com>
2026-08-18 10:37:14 +0600

    samurai: sensors update for A16 QPR2
    samurai: sensors: multihal -> samurai

--- device.mk
+++ device.mk
@@ -467,7 +467,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.samurai \
     libdumpstateutil.vendor:64 \
     libsensorndkbridge \
     sensors.oplus \
